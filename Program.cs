ea79a30e41ccae3eda8591e040c82d21050be77f
Claudiu HBann <123uidualcnnamreh@gmail.com>
2022-06-08 01:28:13 +0300

    added first and last page on pagination for movies and video games if the pages actually makes sense

--- Program.cs
+++ Program.cs
@@ -40,6 +40,16 @@ namespace MvcMovie {
 
 /*
     TO DO:
+---------------------------------------------------------------------
+        2) De adaugat posibilitatea de a insera recenzii / review-uri.
+        
+        In view-ul de detalii, imediat sub proprietatile unui movie, se va implementa un tabel cu review-urile utilizatorilor (nume utilizator, data completarii si recenzia        propriu-zisa) iar sub tabel, se vor adauga 3 elemente noi pentru adaugare de review:
+            - un input unde se va completa numele persoanei care face review-ul;
+            - un input unde se va completa review-ul propriu-zis;
+            - un buton pentru inserarea acestuia.
+        
+        De indata ce se face inserarea, tabelul cu recenzii trebuie sa fie actualizat si sa afiseze si noua inregistrare inserata
+---------------------------------------------------------------------
+
         - generate more realistic data
-        - on pagination add first and last page if it makes sense
  */
